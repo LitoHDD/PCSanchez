710f7068b6de294ded7957f1dd8149799ad878fc
LitoHDD <sergio098890@gmail.com>
2023-05-09 01:05:11 +0200

    Servlet Improvements

--- programming/java/PCSanchez/src/main/java/controlador/LogoutServlet.java
+++ programming/java/PCSanchez/src/main/java/controlador/LogoutServlet.java
@@ -6,22 +6,17 @@ package controlador;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import dao.UsuarioDAO;
-import dto.Usuario;
 
 /**
  *
  * @author jrmd
  */
-public class LoginServlet extends HttpServlet {
+public class LogoutServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -33,30 +28,10 @@ public class LoginServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            response.setContentType("text/html;charset=UTF-8");
-            String email = request.getParameter("email");
-            String password = request.getParameter("password");
-            // Hacemos uso de logica de negocio del bean (dto) como el método "validar()"
-            Usuario usuario = new UsuarioDAO().validar(email, password);
-
-            if (usuario != null) {
-                // Si el login es OK guardamos el objeto de tipo "Usuario" en la sesión para poder recuperarlo más tarde.
-                // Y tras esto redirigimos a "index.html"
-                HttpSession session = request.getSession();
-                session.setAttribute("usuario", usuario);
-                session.setAttribute("username", usuario.getNombreCompleto()); // nombre es el atributo que contiene el nombre de usuario en tu objeto Usuario
-                session.setAttribute("loggedIn", true);
-                response.sendRedirect("index.jsp");
-
-            } else {
-                // Si el login no es correcto, enviamos un atributo "error" a true a "login.jsp" para que muestre el error.
-                request.setAttribute("error", true);
-                request.getRequestDispatcher("login.jsp").include(request, response);
-            }
-        } catch (SQLException ex) {
-            System.out.println("ERROR SQL");
-        }
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session=request.getSession();  
+        session.invalidate();  
+        response.sendRedirect("index.jsp");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
